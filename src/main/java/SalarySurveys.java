957077e7840b5fba7d6f9f7e7729464543063ed6
muma <18551626906@163.com>
2017-08-07 17:42:01 +0800

    Initial commit

---
+++ src/main/java/SalarySurveys.java
@@ -0,0 +1,17 @@
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * 
+ */
+public class SalarySurveys implements PageProcessor{
+
+    public void process(Page page) {
+
+    }
+
+    public Site getSite() {
+        return null;
+    }
+}
